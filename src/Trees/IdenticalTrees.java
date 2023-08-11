8edbbacddbf541856b2d99964456548bc4be24f9
adityasharma <adityamaverick8796@gmail.com>
2023-08-11 16:47:44 +0530

    ZigZagTraversal and IdenticalTrees

---
+++ src/Trees/IdenticalTrees.java
@@ -0,0 +1,41 @@
+package Trees;
+
+public class IdenticalTrees {
+
+    static class Node{
+        Node left, right;
+        int data;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static boolean isIdentical(Node root1, Node root2)
+    {
+        if(root1 == null && root2 == null)
+            return true;
+        if(root1!=null && root2!=null)
+        {
+            return (root1.data == root2.data
+            && isIdentical(root1.left, root2.left)
+            && isIdentical(root1.right, root2.right));
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Node root1 = new Node(1);
+        root1.left = new Node(2);
+        root1.right = new Node(3);
+        root1.left.left = new Node(4);
+        root1.left.right = new Node(5);
+
+        Node root2 = new Node(1);
+        root2.left = new Node(2);
+        root2.right = new Node(3);
+        root2.left.left = new Node(4);
+        root2.left.right = new Node(5);
+
+        System.out.println(isIdentical(root1, root2));
+    }
+}
---
+++ src/Trees/ZigZagTraversal.java
@@ -0,0 +1,62 @@
+package Trees;
+
+import java.util.Stack;
+
+public class ZigZagTraversal {
+
+    static class Node{
+        int data;
+        Node left, right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void zigZagTraversal(Node root)
+    {
+        Stack<Node> curentLevel = new Stack<>();
+        Stack<Node> nextLevel = new Stack<>();
+
+        boolean leftToRight = true;
+        curentLevel.push(root);
+
+        while(!curentLevel.isEmpty()){
+            Node currentNode = curentLevel.pop();
+            System.out.println(currentNode.data);
+            if(leftToRight){
+                if(currentNode.left !=null)
+                    nextLevel.push(currentNode.left);
+                if(currentNode.right != null)
+                    nextLevel.push(currentNode.right);
+            }
+            else{
+                if(currentNode.right != null)
+                    nextLevel.push(currentNode.right);
+                if(currentNode.left != null)
+                    nextLevel.push(currentNode.left);
+            }
+
+            if(curentLevel.isEmpty()){
+                leftToRight = !leftToRight;
+                Stack<Node> temp = new Stack<>();
+                curentLevel = nextLevel;
+                nextLevel = temp;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(7);
+        root.left.right = new Node(6);
+        root.right.left = new Node(5);
+        root.right.right = new Node(4);
+
+        zigZagTraversal(root);
+    }
+}
