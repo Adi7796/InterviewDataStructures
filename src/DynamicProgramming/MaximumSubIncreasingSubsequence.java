7bd00103244a93eac297a6008fbfbe0bf7bd6629
adityasharma <adityamaverick8796@gmail.com>
2023-10-24 15:45:16 +0530

    Maximum increasing subsequence

---
+++ src/DynamicProgramming/MaximumSubIncreasingSubsequence.java
@@ -0,0 +1,32 @@
+package DynamicProgramming;
+
+public class MaximumSubIncreasingSubsequence {
+    public static void main(String[] args) {
+        int[] arr = {1, 101, 2, 3, 100, 4, 5};
+
+        System.out.println("The maximum sum increasing subsequence is :"+ findMaximumSum(arr));
+    }
+
+    public static int findMaximumSum(int[] arr) {
+        int n = arr.length;
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = arr[i];
+        }
+        int max_result =0;
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(arr[i] > arr[j] && dp[i] < dp[j] + arr[i]){
+                    dp[i] = dp[j] + arr[i];
+                }
+                max_result = Math.max(max_result, dp[i]);
+            }
+        }
+
+        return max_result;
+    }
+}
