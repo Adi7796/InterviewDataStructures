5b2b0f15a80538404bc881462921fc927af4e0bd
adityasharma <adityamaverick8796@gmail.com>
2023-11-14 15:13:33 +0530

    Longest palindromic substring

--- src/DynamicProgramming/LongestPalindromicSubstring.java
+++ src/DynamicProgramming/LongestPalindromicSubstring.java
@@ -5,16 +5,19 @@ public class LongestPalindromicSubstring {
         String str = "abdbca";
         String str2 = "forgeeksskeegfor";
 
-        System.out.println("Max length for "+ str + " : "+ findMaxLength(str));
-        System.out.println("Max length for " + str2 + " : " + findMaxLength(str2));
+        String strAns = findMaxLength(str);
+        String strAns2 = findMaxLength(str2);
+        System.out.println("Max length for "+ strAns + " : "+ strAns.length());
+        System.out.println("Max length for " + strAns2 + " : " + strAns2.length());
     }
 
-    public static int findMaxLength(String str)
+    public static String findMaxLength(String str)
     {
         boolean[][] dp = new boolean[str.length()][str.length()];
         int n = str.length();
         int maxLength = 0;
         int count = 0;
+        StringBuffer strbuff = new StringBuffer("");
         for(int gap =0; gap < n; gap ++)
         {
             for(int i=0,j=gap ; j< n; i++, j++)
@@ -22,12 +25,14 @@ public class LongestPalindromicSubstring {
                 if(gap ==0){   // if we are at the same character
                     dp[i][j] = true;
                     maxLength = 1;
+                    strbuff = new StringBuffer(str.substring(i,j+1));
                 }
                 else if(gap == 1)
                 {
                     if(str.charAt(i) == str.charAt(j)){   // for length = 2 and the gap between start and end pointer is 1
                         dp[i][j] = true;
                         maxLength = 2;
+                        strbuff = new StringBuffer(str.substring(i,j+1));
                     }
 
                     else dp[i][j] = false;
@@ -37,6 +42,7 @@ public class LongestPalindromicSubstring {
                     if(str.charAt(i) == str.charAt(j) && dp[i+1][j-1] == true){
                         dp[i][j] = true;
                         maxLength = gap + 1;
+                        strbuff = new StringBuffer(str.substring(i,j+1));
                     }
                     else
                         dp[i][j] = false;
@@ -46,6 +52,6 @@ public class LongestPalindromicSubstring {
                     count ++;    // to keep count of the number of substring palindromes
             }
         }
-        return maxLength;
+        return strbuff.toString();
     }
 }
--- src/DynamicProgramming/MaximumSumIncreasingSubsequence.java
+++ src/DynamicProgramming/MaximumSumIncreasingSubsequence.java
@@ -1,6 +1,6 @@
 package DynamicProgramming;
 
-public class MaximumSubIncreasingSubsequence {
+public class MaximumSumIncreasingSubsequence {
     public static void main(String[] args) {
         int[] arr = {1, 101, 2, 3, 100, 4, 5};
 
