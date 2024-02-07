44bc27df674b5e140365a0e97c4e154e5ed7027d
adityasharma <adityamaverick8796@gmail.com>
2024-02-08 00:17:20 +0530

    Builder design pattern

--- src/Design/AbstractDesignFactoryPattern/EmployeeAbstractFactory.java
+++ src/Design/AbstractDesignFactoryPattern/EmployeeAbstractFactory.java
@@ -1,5 +1,7 @@
 package Design.AbstractDesignFactoryPattern;
 
+// inserting another layer after factory.
+// So client -> factory -> abstract factory -> object creation
 public abstract class EmployeeAbstractFactory {
     public abstract Employee getEmployee();
 }
--- src/Design/BuilderDesignPattern/Client.java
+++ src/Design/BuilderDesignPattern/Client.java
@@ -1,4 +1,14 @@
 package Design.BuilderDesignPattern;
 
 public class Client {
+    public static void main(String[] args) {
+
+        User user = new User.UserBuilder()
+                .setUserId(1234)
+                .setUserName("Aditya")
+                .setEmailId("adit@123.com")
+                .build();
+
+        System.out.println(user);
+    }
 }
