3c1c729b551195033312f77dace7d7ceefbfaf27
adityasharma <adityamaverick8796@gmail.com>
2024-02-08 19:07:11 +0530

    Hash code and equals contract

---
+++ src/Design/HashCodeAndEqualsContract/Pen.java
@@ -0,0 +1,32 @@
+package Design.HashCodeAndEqualsContract;
+
+import java.util.Objects;
+
+public class Pen {
+
+    private int price;
+    private String color;
+
+    public Pen(int price, String color) {
+        this.price = price;
+        this.color = color;
+    }
+
+    //Uncomment th below code to define the logic of equals
+//    @Override
+//    public boolean equals(Object obj){
+//        Pen that = (Pen)obj;
+//
+//        boolean isEqual = this.price == that.price && this.color.equals(that.color);
+//        return isEqual;
+//    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pen pen = (Pen) o;
+        return price == pen.price && Objects.equals(color, pen.color);
+    }
+}
