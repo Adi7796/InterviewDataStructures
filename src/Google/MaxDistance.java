f71b4ad0657bf8d0c5d6fd371d25ad9d9d2b7783
adityasharma <adityamaverick8796@gmail.com>
2024-02-20 00:16:15 +0530

    Google Interview Questions

---
+++ src/Google/MaxDistance.java
@@ -0,0 +1,59 @@
+package Google;
+
+import java.util.Arrays;
+
+/*
+You have been given an array 'A' of N integers. You need to find the maximum value of j - i
+subjected to the constraint of A[i] <= A[j], where ‘i’ and ‘j’ are the indices of the array.
+
+For example :
+If 'A' = {3, 5, 4, 1}
+
+then the output will be 2.
+Maximum value occurs for the pair (3, 4)
+ */
+public class MaxDistance {
+    public static void main(String[] args) {
+        int[] arr = {34, 8, 10, 3 ,2 ,80 ,30 ,33 ,1};
+        int n = arr.length;
+
+        int[] leftMin = new int[n];
+        int[] rightMax = new int[n];
+
+        Arrays.fill(leftMin, Integer.MAX_VALUE);
+        Arrays.fill(rightMax, Integer.MIN_VALUE);
+
+        leftMin[0] = arr[0];
+        rightMax[n-1] = arr[n-1];
+
+        for(int i=1, j=n-2; i<n && j>=0; i++, j--)
+        {
+            leftMin[i] = Math.min(leftMin[i-1], arr[i]);
+            rightMax[j] = Math.max(rightMax[j+1], arr[j]);
+        }
+
+        for(int i : leftMin)
+            System.out.print(i + " ");
+
+        System.out.println();
+        for(int j : rightMax)
+            System.out.print(j + " ");
+        System.out.println();
+
+        int i=0, j=0;
+        int maxDistance = Integer.MIN_VALUE;
+        while(i<n && j< n)
+        {
+            if(leftMin[i] <= rightMax[j]){
+                maxDistance = Math.max(maxDistance, j-i);
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+
+        System.out.println("Max Distance : " + maxDistance);
+        // Maximum value occurs for the pair (8, 33)
+    }
+}
---
+++ src/Google/PascalTriangle.java
@@ -0,0 +1,37 @@
+package Google;
+
+import java.util.ArrayList;
+
+public class PascalTriangle {
+    public static void main(String[] args) {
+        int N = 9;
+        ArrayList<ArrayList<Integer>> pascal = printPascal(N);
+        for(ArrayList<Integer> rowList : pascal)
+        {
+            for(Integer i : rowList){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static ArrayList<ArrayList<Integer>> printPascal(int n) {
+        // Write your code here.
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for(int i=1; i<=n; i++)
+        {
+            ArrayList<Integer> rowList = new ArrayList<>();
+            for(int j=1;j<=i;j++)
+            {
+                if(j==1 || j==i) rowList.add(1);
+                else if(i>=3 && j>1 && j<n){
+                    int x = ans.get(i-2).get(j-2);
+                    int y = ans.get(i-2).get(j-1);
+                    rowList.add(x+y);
+                }
+            }
+            ans.add(rowList);
+        }
+        return ans;
+    }
+}
