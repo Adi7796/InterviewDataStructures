81dcf66630b02fb46cb2714947e0a2d34c24b0fb
adityasharma <adityamaverick8796@gmail.com>
2024-02-21 00:20:46 +0530

    Single Number

---
+++ src/Google/SingleNumber.java
@@ -0,0 +1,30 @@
+package Google;
+
+import java.util.Arrays;
+
+/*
+You are given an arbitrary array ‘arr’ consisting of N non-negative integers,
+where every element appears thrice except one.
+You need to find the element that appears only once.
+ */
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 3, 3, 3, 6};
+        Arrays.sort(arr);
+
+        for(int i=0; i<arr.length; i++)
+        {
+            int count =1;
+            while(i<arr.length - 1 && arr[i+1] == arr[i])
+            {
+                count++;
+                i++;
+            }
+            if(count ==1)
+            {
+                System.out.println("Single number : " + arr[i]);
+                return;
+            }
+        }
+    }
+}
