37a21f1b7b72872e42b3fbf5ba9965b0d82e1751
adityasharma <adityamaverick8796@gmail.com>
2024-03-01 20:17:02 +0530

    Jumping Numbers

---
+++ src/Google/JumpingNumbers.java
@@ -0,0 +1,63 @@
+package Google;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+A number is called as a Jumping Number if all adjacent digits in it differ by 1.
+The difference between ‘9’ and ‘0’ is not considered as 1.
+All single digit numbers are considered as Jumping Numbers.
+For example 7, 8987 and 4343456 are Jumping numbers but 796 and 89098 are not.
+Given a positive number x, print all Jumping Numbers smaller than or equal to x.
+The numbers can be printed in any order.
+ */
+public class JumpingNumbers {
+    public static void main(String[] args) {
+        int N = 500;
+
+        for(int i=1;i<=9;i++)
+        {
+            BFS(N, i);
+        }
+    }
+
+    public static void BFS(int N, int i)
+    {
+        Queue<Integer> queue= new LinkedList<>();
+
+        queue.add(i);
+
+        while(!queue.isEmpty())
+        {
+            int num = queue.poll();
+
+            if(num<N){
+                int last_digit = num % 10;
+
+                System.out.print(num + " ");
+                if(last_digit == 0){
+                    queue.add((num*10) + (last_digit + 1));
+                }
+                else if(last_digit == 9)
+                {
+                    queue.add((num*10) + (last_digit -1));
+                }
+                else{
+                    queue.add((num*10) + (last_digit -1));
+                    queue.add((num*10) + (last_digit + 1));
+                }
+            }
+        }
+    }
+}
+
+/*
+Time Complexity: O(k)
+
+Here k is the total number of jumping numbers.
+
+Auxiliary Space: O(len(N))
+
+Here len(N) is the maximum length from all the jumping numbers,
+the extra space is used due to recursive function call stack.
+ */
