e6cff231cc3abefe4e20c312496de857ff654244
adityasharma <adityamaverick8796@gmail.com>
2024-06-16 13:31:16 +0530

    Google questions

--- src/Java_8_Features/CollectionImprovements/MapImprovementFetchOperations.java
+++ src/Java_8_Features/CollectionImprovements/MapImprovementFetchOperations.java
@@ -1,7 +1,12 @@
 package Java_8_Features.CollectionImprovements;
 
-public class MapImprovementFetchOperations {
-    public static void main(String[] args) {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class MapImprovementFetchOperations {
+
+    static Queue<Integer> queue = new LinkedList<>();
+    public static void main(String[] args) {
+        queue.poll();
     }
 }
