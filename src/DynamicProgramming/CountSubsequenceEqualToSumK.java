549fea78e2d3bc43c1d1fc5c1923210c24d4932e
adityasharma <adityamaverick8796@gmail.com>
2025-04-28 18:58:03 +0530

    Minor changes

--- src/DynamicProgramming/CountSubsequenceEqualToSumK.java
+++ src/DynamicProgramming/CountSubsequenceEqualToSumK.java
@@ -1,5 +1,18 @@
 package DynamicProgramming;
 
+/*
+Given an array nums and an integer k.Return the number of non-empty subsequences of nums
+such that the sum of all elements in the subsequence is equal to k.
+
+Examples:
+Input : nums = [4, 9, 2, 5, 1] , k = 10
+Output : 2
+Explanation : The possible subsets with sum k are [9, 1] , [4, 5, 1].
+
+Input : nums = [4, 2, 10, 5, 1, 3] , k = 5
+Output : 3
+Explanation : The possible subsets with sum k are [4, 1] , [2, 3] , [5].
+ */
 public class CountSubsequenceEqualToSumK {
     public static void main(String[] args) {
         int[] nums = {5, 2, 3, 10, 6, 8};
