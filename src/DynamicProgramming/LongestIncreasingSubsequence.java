7c944118b9ce6c2cf2123e20313f5ae1d7fd9064
adityasharma <adityamaverick8796@gmail.com>
2026-07-14 18:50:47 +0530

    dp problems

--- src/DynamicProgramming/LongestIncreasingSubsequence.java
+++ src/DynamicProgramming/LongestIncreasingSubsequence.java
@@ -7,16 +7,56 @@ public class LongestIncreasingSubsequence {
         //int[] arr = {3, 10, 2, 1, 20};
         int[] arr = {50, 3, 10, 7, 40, 80};
 
-        System.out.println("Longest increasing subsequence : "+ findLongest(arr));
+        System.out.println("Longest increasing subsequence : "+ tabulation(arr));
     }
 
-    public static int findLongest(int[] arr)
+    static int lis(int arr[]) {
+        // code here
+        int n = arr.length;
+        int[][] dp = new int[n][n+1];
+        for(int i = 0; i<n; i++)
+        {
+            Arrays.fill(dp[i], -1);
+        }
+        //return recurse(arr,0,-1);
+        //return memoization(arr, 0, -1, dp);
+        return tabulation(arr);
+    }
+
+    private static int recurse(int[] arr, int currInd, int prevInd)
+    {
+        if(currInd == arr.length) return 0;
+
+        int take = Integer.MIN_VALUE;
+        if(prevInd == -1 || arr[currInd] > arr[prevInd]){
+            take = 1 + recurse(arr, currInd + 1, currInd);
+        }
+        int notTake = recurse(arr, currInd + 1, prevInd);
+
+        return Math.max(take, notTake);
+    }
+
+    private static int memoization(int[] arr, int currInd, int prevInd, int[][] dp)
+    {
+        if(currInd == arr.length) return 0;
+
+        if(dp[currInd][prevInd+1] != -1) return dp[currInd][prevInd+1];
+        int take = Integer.MIN_VALUE;
+        if(prevInd == -1 || arr[currInd] > arr[prevInd]){
+            take = 1 + memoization(arr, currInd + 1, currInd, dp);
+        }
+        int notTake = memoization(arr, currInd + 1, prevInd, dp);
+
+        return dp[currInd][prevInd+1]= Math.max(take, notTake);
+    }
+
+    public static int tabulation(int[] arr)
     {
         int[] dp = new int[arr.length];
 
         Arrays.fill(dp, 1);
         dp[0] = 1;
-        int max_result= 0;
+        int max_result= 1;
         for(int i=1; i<arr.length; i++)
         {
             for(int j=0; j<i; j++)
