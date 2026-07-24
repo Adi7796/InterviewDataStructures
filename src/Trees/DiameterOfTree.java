c4578fb7b914751ab1cfb9cf5903cb64ffb4977d
adityasharma <adityamaverick8796@gmail.com>
2026-07-24 13:06:06 +0530

    trees problems

--- src/Trees/DiameterOfTree.java
+++ src/Trees/DiameterOfTree.java
@@ -1,7 +1,9 @@
 package Trees;
 
 /*
-The diameter/width of a tree is defined as the number of nodes on the longest path between two end no des
+Given the root of a binary tree, your task is to find the diameter of the binary tree.
+The diameter of a binary tree is defined as the number of edges on the longest path between any two nodes.
+Note that this path may or may not pass through the root of the tree.
  */
 public class DiameterOfTree {
     static class Height{
@@ -11,7 +13,7 @@ public class DiameterOfTree {
         CreateBinarySearchTree.Node root = HeightOfBinaryTree.createBinaryTree();
         System.out.println("Diameter of the tree : "+ findDiameter(root));
         Height height = new Height();
-        System.out.println("Diameter of tree using efficient approach : " + findDiameter1(root, height));
+        System.out.println("Diameter of tree using efficient approach : " + diameter(root));
     }
 
     //Time Complexity : O(n2)  -> since for every node, we are calling the height -> so for n nodes, n can be the height
@@ -38,21 +40,37 @@ public class DiameterOfTree {
 
     // efficient Approach O(N)
 
-    public static int findDiameter1(CreateBinarySearchTree.Node root, Height height)
-    {
-        Height leftHeight = new Height();
-        Height rightHeight = new Height();
+    public static int diameter(CreateBinarySearchTree.Node root) {
+        // code here
+        // since java is pas by value and not reference, we create the array so that the value doesn't disappear
+        // when the recursion returns to the prev call - small java trick
+        int[] maxi = new int[1];
+        maxDiameter(root, maxi);
+        return maxi[0];
+    }
 
-        if(root == null)
-        {
-            height.h = 0;
+
+    /*
+     * Returns the height of the current subtree while simultaneously
+     * updating the maximum diameter found so far.
+     *
+     * Diameter = Number of edges on the longest path between any two nodes.
+     */
+    private static int maxDiameter(CreateBinarySearchTree.Node root, int[] maxi) {
+
+        // Base case: height of an empty tree is 0
+        if (root == null)
             return 0;
-        }
 
-        int leftDiameter = findDiameter1(root.left, leftHeight);
-        int rightDiameter = findDiameter1(root.right, rightHeight);
+        // Recursively compute heights of left and right subtrees
+        int leftHeight = maxDiameter(root.left, maxi);
+        int rightHeight = maxDiameter(root.right, maxi);
 
-        height.h = Math.max(leftHeight.h , rightHeight.h )+ 1;
-        return Math.max(leftHeight.h + rightHeight.h + 1, Math.max(leftDiameter, rightDiameter));
+        // Diameter passing through the current node =
+        // height of left subtree + height of right subtree
+        maxi[0] = Math.max(maxi[0], leftHeight + rightHeight);
+
+        // Return height of the current subtree
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 }
